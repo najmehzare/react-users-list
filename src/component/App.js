48bae45727769dc1a25dc559c82856bbfb7df017
“Najmeh <n.zare.98@gmail.com>
2022-06-22 13:02:25 +0430

    feat: add init tailwind

--- src/component/App.js
+++ src/component/App.js
@@ -4,9 +4,9 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-               
-            </div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </React.StrictMode>
 );
 
