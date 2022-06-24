fe422194882d732a954669e3816c5f86bd034407
“Najmeh <n.zare.98@gmail.com>
2022-06-24 08:16:56 +0430

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-import App from './component/app';
+import App from './component/App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from "@material-tailwind/react";
 
