98632cc94107999a254fb61375f10b30fc61d353
“Najmeh <n.zare.98@gmail.com>
2022-06-27 11:54:17 +0430

    feat: add lazy loading

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
-import React , { Component } from 'react';
+import React , { Component , lazy , Suspense} from 'react';
+import { Routes , Route } from 'react-router-dom';
 
 import Header from './Admin/Layouts/header';
 
@@ -6,16 +7,18 @@ import authContext from '../Contexts/authContext';
 
 //import routers
 import Users from '../Routes/users';
-import About from '../Routes/about';
-import { Routes , Route } from 'react-router-dom';
-import Contact from '../Routes/Contact/contact';
+// import About from '../Routes/about';
+// import Articles from '../Routes/Articles';
+// import Contact from '../Routes/Contact/contact';
 import ContactForm from '../Routes/Contact/contactForm';
-import Articles from '../Routes/Articles';
 import SingleArticle from '../Routes/Articles/singleArticle';
 import ContactManager from '../Routes/Contact/contactManager';
 import NotFound from '../Routes/notFound';
 
-
+//lazy loading
+const About = lazy(() => import('../Routes/about'));
+const Contact = lazy(() => import('../Routes/Contact/contact'));
+const Articles = lazy(() => import('../Routes/Articles'));
 
 class App extends Component {
 
@@ -35,13 +38,20 @@ class App extends Component {
                     <Header />
                     <Routes>
                         <Route path='/' element={ <Users />} />
-                        <Route path='/aboutUs' element={ <About />} />
-                        <Route path='/contactUs' element={ <Contact />} >
+                        <Route path='/aboutUs' element={ 
+                            <Suspense>
+                                <About />
+                            </Suspense>
+                        } />
+                        <Route path='/contactUs' element={ 
+                            <Suspense>
+                               <Contact />
+                            </Suspense>
+                        } >
                             <Route path='' element={<ContactForm />} />
                             <Route path='manager' element={ <ContactManager />} />
-                            
                         </Route>    
-                        <Route path='/articles' element={ <Articles />} />
+                        <Route path='/articles' element={ <Suspense ><Articles /></Suspense>} />
                         <Route path='/articles/:id' element={ <SingleArticle />} />
                         <Route path='*' element={<NotFound />} />
                     </Routes>
